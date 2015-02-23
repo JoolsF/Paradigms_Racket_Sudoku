0971ffa19b435cfb179b24aead667ec9706acf41
joolsmf <joolsmf@gmail.com>
2015-02-23 17:34:12 +0000

    singleton matches successfully removed from rows.  Now need to apply to columns and squares

--- matrix.rkt
+++ matrix.rkt
@@ -1,5 +1,7 @@
 #lang racket
-(require srfi/1)
+(require racket/include)
+(include "data.rkt")
+
 
 ;;;NOTES TO DELETE
 ; (member "fall" (list "hop" "skip" "jump"))
@@ -8,19 +10,6 @@
 ; (list-index (curry equal? 2) (car (transform matrix)))
 ;;END OF NOTES TO DELETE
 
-
-(define matrix 
-  (list (list 0 2 5 0 0 1 0 0 0)
-        (list 1 0 4 2 5 0 0 0 0)
-        (list 0 0 6 0 0 4 2 1 0)
-        (list 0 5 0 0 0 0 3 2 0)
-        (list 6 0 0 0 2 0 0 0 9)
-        (list 0 8 7 0 0 0 0 6 0)
-        (list 0 9 1 5 0 0 6 0 0)
-        (list 0 0 0 0 7 8 1 0 3)
-        (list 0 0 0 6 0 0 5 9 0)
-        )
-  )
 
 ;; The set {1-9}
 (define sudokuset(list 1 2 3 4 5 6 7 8 9))
@@ -33,7 +22,7 @@
       (list x))
   )
 
-
+; TO DO - Refactor these functions, see http://stackoverflow.com/questions/5699899/scheme-map-function-for-applying-a-function-to-elements-in-a-nested-list
 ;; For mapping each matrix row's element to function f
 (define (map-matrix f lst)
   (map (lambda (x) 
@@ -48,8 +37,7 @@
 
 
 
-
-
+;;takes a list of lists and returns  lists with a single element
 (define (find-singleton lst)
   (acc-find-singleton lst '()))
 
@@ -59,23 +47,28 @@
    [(= (length (car lst)) 1)  (acc-find-singleton (cdr lst) (append resLst (car lst)  ))]
    [else (acc-find-singleton (cdr lst) resLst)]))
   
- 
+(define (remove-singleton-matches-rows matrix)
+  (map (lambda (x)
+         (cond
+           [(= (length x) 1) x]
+           [else (remove* (find-singleton matrix) x)]))
+         matrix))
+
+
+
+
+(define (solve matrix)
+  (map (lambda (x) 
+         (remove-singleton-matches-rows x)) 
+       matrix))
 
 
 ;; Execution starts here
 ;; (solve matrix)
 ;; (transform matrix)
 
-;(find-singleton (car(transform matrix)))
 
 
 
-;;this needs refactoring, hard to read
-(map (lambda (x)
-       (cond
-         [(= (length x) 1) x]
-         [else (remove* (find-singleton (car(transform matrix))) x)])) 
-     (car(transform matrix)))
-
-
+;(solve (transform matrix))
 
--- test1.rkt
+++ test1.rkt
@@ -3,6 +3,20 @@
 (define jlist 
   (list (list 1 2 3) (list 2) (list 1 2 3)))
 
+(define test-matrix 
+  (list (list 0 2 5 0 0 1 0 0 0)
+        (list 1 0 4 2 5 0 0 0 0)
+        (list 0 0 6 0 0 4 2 1 0)
+        (list 0 5 0 0 0 0 3 2 0)
+        (list 6 0 0 0 2 0 0 0 9)
+        (list 0 8 7 0 0 0 0 6 0)
+        (list 0 9 1 5 0 0 6 0 0)
+        (list 0 0 0 0 7 8 1 0 3)
+        (list 0 0 0 6 0 0 5 9 0)
+        )
+  )
+
+
 
 
 ;; test 1
@@ -17,12 +31,12 @@
   
 
 ;;test 2
-(map (lambda (x)
-       (cond
-         [(= (length x) 1) x]
-         [else (remove* (find-singleton jlist) x)])) 
-     jlist)
+;(map (lambda (x)
+;       (cond
+;         [(= (length x) 1) x]
+;         [else (remove* (find-singleton jlist) x)])) 
+;     jlist)
 
 
-;;test 3 - mapping a nested list structure
+;;test 3 - Traversing a 2d matrix
 
