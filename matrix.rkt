e0147d1f65f0a704cb416c047548f51bfd1a68db
joolsmf <joolsmf@gmail.com>
2015-02-23 18:28:16 +0000

    tidied up code and used deep-map (found on stackoverflow) to abstract the mapping of 2d lists for effective reuse

--- matrix.rkt
+++ matrix.rkt
@@ -1,43 +1,20 @@
 #lang racket
 (require racket/include)
 (include "data.rkt")
+(include "my-helper-functions.rkt")
 
 
-;;;NOTES TO DELETE
-; (member "fall" (list "hop" "skip" "jump"))
-;  once all sets of numbers are reduced to length 1, flatten each row into original format of Matrix
-;  http://stackoverflow.com/questions/15871042/how-do-i-find-the-index-of-an-element-in-a-list-in-racket
-; (list-index (curry equal? 2) (car (transform matrix)))
-;;END OF NOTES TO DELETE
-
-
-;; The set {1-9}
-(define sudokuset(list 1 2 3 4 5 6 7 8 9))
-
-
-;;replaces zeroes in list with sudokuset
-(define (replace-empty x)
-  (if (= x 0)
-      sudokuset
-      (list x))
-  )
-
-; TO DO - Refactor these functions, see http://stackoverflow.com/questions/5699899/scheme-map-function-for-applying-a-function-to-elements-in-a-nested-list
-;; For mapping each matrix row's element to function f
-(define (map-matrix f lst)
-  (map (lambda (x) 
-         (f x)) 
-       lst))
-
-;;tranforms matrix using replace-empty
+;; Tranforms matrix by replacing all zeroes in a list with the (list 1-9) and all other values as a singleton list
 (define (transform matrix)
-  (map (lambda (x) 
-         (map-matrix replace-empty x)) 
-       matrix))
+  (deep-map (lambda (x) 
+              (cond
+                [(= x 0)(list 1 2 3 4 5 6 7 8 9) ]
+                [else (list x)])) 
+            matrix))
 
 
 
-;;takes a list of lists and returns  lists with a single element
+;;takes a list of lists and returns the contents of all singletons as a list 
 (define (find-singleton lst)
   (acc-find-singleton lst '()))
 
@@ -47,28 +24,31 @@
    [(= (length (car lst)) 1)  (acc-find-singleton (cdr lst) (append resLst (car lst)  ))]
    [else (acc-find-singleton (cdr lst) resLst)]))
   
-(define (remove-singleton-matches-rows matrix)
+(define (remove-singleton-from-sets matrix)
   (map (lambda (x)
          (cond
            [(= (length x) 1) x]
            [else (remove* (find-singleton matrix) x)]))
          matrix))
 
-
-
-
 (define (solve matrix)
   (map (lambda (x) 
-         (remove-singleton-matches-rows x)) 
+         (remove-singleton-from-sets x)) 
        matrix))
 
 
-;; Execution starts here
-;; (solve matrix)
-;; (transform matrix)
+
+;; PROGRAM EXCECUTION
+(solve (transform matrix))
 
 
 
 
-;(solve (transform matrix))
 
+
+;;;NOTES TO DELETE
+; (member "fall" (list "hop" "skip" "jump"))
+;  once all sets of numbers are reduced to length 1, flatten each row into original format of Matrix
+;  http://stackoverflow.com/questions/15871042/how-do-i-find-the-index-of-an-element-in-a-list-in-racket
+; (list-index (curry equal? 2) (car (transform matrix)))
+;;END OF NOTES TO DELETE
--- test2.rkt
+++ test2.rkt
@@ -1,46 +1,12 @@
 #lang racket
 
-(provide jlist)
-
-;deep-map function taken from  http://stackoverflow.com/questions/5699899/scheme-map-function-for-applying-a-function-to-elements-in-a-nested-list
-; worked out concept of mapping a map until the inner element is discovered by couldn't find optimal way of calling it
-(define (deep-map f l)
-  (let deep ((x l))
-    (cond ((null? x) x)
-          ((pair? x) (map deep x))
-          (else (f x)))))
-
-
-(define jlist 
-  (list (list 1 2 3) (list 2) (list 1 2 3)))
-      
-
-(define matrix 
-  (list (list 0 2 5 0 0 1 0 0 0)
-        (list 1 0 4 2 5 0 0 0 0)
-        (list 0 0 6 0 0 4 2 1 0)
-        (list 0 5 0 0 0 0 3 2 0)
-        (list 6 0 0 0 2 0 0 0 9)
-        (list 0 8 7 0 0 0 0 6 0)
-        (list 0 9 1 5 0 0 6 0 0)
-        (list 0 0 0 0 7 8 1 0 3)
-        (list 0 0 0 6 0 0 5 9 0)
-        )
-  )
-
-;; The set {1-9}
+(require racket/include)
+(include "data.rkt")
 (define sudokuset(list 1 2 3 4 5 6 7 8 9))
 
 
-
-
-
-;;replaces zeroes in list with sudokuset
-(define (replace-empty x)
-  (if (= x 0)
-      sudokuset
-      (list x))
-  )
-
-
+;;test iterate columns
+;(define (iterate-column lst)
+ ; (case
+ ;     [(empty? lst) #f]
 
