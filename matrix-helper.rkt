435ce2a04a9094dcb11a9e48ff69779c7e3cde89
joolsmf <joolsmf@gmail.com>
2015-03-02 12:17:02 +0000

    almost solved column traversal issues

---
+++ matrix-helper.rkt
@@ -0,0 +1,51 @@
+
+
+
+;; HELPER FUNCTIONS - To move other module
+;; deep-map taken from http://stackoverflow.com/questions/5699899/scheme-map-function-for-applying-a-function-to-elements-in-a-nested-list
+;; as can be seen from github history, I'd been using a process of nested maps to iterate a 2d list but needed an efficient way of abstracting this
+;; to avoid code repetition and to increase readability
+(define (deep-map-innermost f l)
+  (let deep ((x l))
+    (cond ((null? x) x)
+          ((pair? x) (map deep x))
+          (else (f x)))))
+
+; Helper method to get nth item of list
+(define (get-item number list)                   
+      (cond 
+        [(null? list) '()]             
+        [(= number 0) (car list)]
+        [else (get-item (- number 1) (cdr list))]
+        )
+  )
+
+
+; TO DO - refactor 
+;Applies function to innermost lists of a matrix
+(define (deep-map-innermost-list f l)
+  (let deep ((x l)) 
+    (cond [(null? x) x]
+          [(and (pair? x) (number? (car x)))
+           (f x)]
+          (else (map deep x)))))
+
+
+(define (zip p q) (map list p q)) 
+
+
+
+
+
+;; END OF HELPER FUNCTIONS
+
+
+
+
+
+
+;ADD COLUMN INDEX TO MATRIX
+(define (add-column-index matrix)
+  (map (lambda (x) 
+         (zip x (list 0 1 2 3 4 5 6 7 8) ))
+       matrix))  ;<-- gives the index
--- matrix.rkt
+++ matrix.rkt
@@ -2,64 +2,10 @@
 
 (require racket/include)
 (include "data.rkt")
-(include "test2.rkt")
+(include "matrix-helper.rkt")
 
 
-;; HELPER FUNCTIONS - To move other module
-;; deep-map taken from http://stackoverflow.com/questions/5699899/scheme-map-function-for-applying-a-function-to-elements-in-a-nested-list
-;; as can be seen from github history, I'd been using a process of nested maps to iterate a 2d list but needed an efficient way of abstracting this
-;; to avoid code repetition and to increase readability
-(define (deep-map-innermost f l)
-  (let deep ((x l))
-    (cond ((null? x) x)
-          ((pair? x) (map deep x))
-          (else (f x)))))
-
-
-; TO DO - refactor 
-;Applies function to innermost lists of a matrix
-(define (deep-map-innermost-list f l)
-  (let deep ((x l)) 
-    (cond [(null? x) x]
-          [(and (pair? x) (number? (car x)))
-           (f x)]
-          (else (map deep x)))))
-
-
-
-
-; Helper method to get nth item of list
-(define (get-item number list)                   
-      (cond 
-        [(null? list) '()]             
-        [(= number 0) (car list)]
-        [else (get-item (- number 1) (cdr list))]
-        )
-  )
-
-;; END OF HELPER FUNCTIONS
-
-
-;; Tranforms matrix by replacing all zeroes in a list with the (list 1-9) and all other values as a singleton list
-(define (transform matrix)
-  (deep-map-innermost (lambda (x) 
-              (cond
-                [(= x 0)(list 1 2 3 4 5 6 7 8 9) ]
-                [else (list x)])) 
-            matrix))
-
-
-
-;ADD COLUMN INDEX TO MATRIX
-(define (add-column-index matrix)
-  (map (lambda (x) 
-         (zip x (list 0 1 2 3 4 5 6 7 8) ))
-       matrix))  ;<-- gives the index
-
-
-
-
-
+;; FIND SINGLETONS AND GATHERS THEM INTO SINGLE LIST
 ;;takes a list of lists and returns the contents of all singletons as a list 
 (define (find-singleton lst)
   (acc-find-singleton lst '()))
@@ -77,6 +23,25 @@
            [else (remove* (find-singleton matrix) x)]))
          matrix))
 
+
+;;Finds singleton in column
+(define (find-singleton-column item-no matrix)
+  (find-singleton (map (lambda (x) (get-item item-no x)) matrix)))
+
+
+
+
+
+;; SETS UP THE SUDOKU  
+;;Tranforms matrix by replacing all zeroes in a list with the (list 1-9) and all other values as a singleton list
+(define (transform matrix)
+  (deep-map-innermost (lambda (x) 
+              (cond
+                [(= x 0)(list 1 2 3 4 5 6 7 8 9) ]
+                [else (list x)])) 
+            matrix))
+
+;;SOLVES THE SUDOKU
 (define (solve matrix)
   (map (lambda (x) 
          (remove-singleton-from-sets x)) 
@@ -94,21 +59,20 @@
 
 
 
-; TO DO - refactor
-(define (zip p q) (map list p q)) 
-
-(define (find-singleton-column item-no matrix)
-  (find-singleton (map (lambda (x) (get-item item-no x)) matrix)))
+;(define (remove-col-dupe matrix)
+ ; (remove-col-dupe-helper(add-column-index matrix) matrix))
+ ; (print (add-column-index matrix)))
 
 
-
-
-(define (remove-col-dupe matrix)
-  (deep-map-innermost-list (lambda (x) 
-                             (remove-if-non-singleton (find-singleton-column 0 matrix) x))
-                           matrix))
+;(define (remove-col-dupe-helper indexed-matrix matrix)
+ ; (map-indexed-matrix (lambda (x) 
+  ;                           (remove-if-non-singleton (find-singleton-column 0 matrix) x))
+       ; print x))
+   ;                        indexed-matrix))
        
 
+
+;;;;TEST AREA
 
 ;;START HERE NEED TO APPLY SOLVE METHODS TO DIFFERENT LIST STRUCTURE
 ;; these are the inner element after ---> (add-column-index (transform matrix))
@@ -116,11 +80,43 @@
 ;; base case is (pair? (cadr TESTLIST)) which is the index
 
 
+
+
+
+
+(define jtemp (add-column-index (transform matrix)))
+
+;(map (lambda (x) (print (append x (list "XXX")))) jtemp)
+
+;(solve (car jtemp))
+
+
+
+
+
+;;;;END OF TEST AREA
+
+
+
+(define JOOLS (list(list(list 1 2 3 4 5 6 7 8 9) 0) (list (list 2) 1) (list(list 5) 2) (list( list 1 2 3 4 5 6 7 8 9) 3) (list (list 1 2 3 4 5 6 7 8 9) 4) (list (list 1) 5) (list ( list 1 2 3 4 5 6 7 8 9) 6) (list (list 1 2 3 4 5 6 7 8 9) 7) (list (list 1 2 3 4 5 6 7 8 9) 8)))
+
+
+
+
+
 (define TESTLIST (list (list 1 2 3 4 5 6 7 8 9) 0))
 
 ;(deep-map-innermost-list (lambda (x) (print x)) jools)
 
 
+
+(solve(add-column-index (transform matrix)))
+
+;(remove-col-dupe (solve(transform matrix)))
+
+;(transform matrix)
+
+;(solve (transform matrix))
 
 
 
