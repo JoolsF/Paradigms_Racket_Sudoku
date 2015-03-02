866be3d195d13bfc8415465230179db2269bd124
joolsmf <joolsmf@gmail.com>
2015-03-02 16:41:16 +0000

    code tidied up.  Filter rows and filter columns now working.  Just need to implement filter-squares now

--- matrix.rkt
+++ matrix.rkt
@@ -6,7 +6,7 @@
 
 
 ;; FIND SINGLETONS AND GATHERS THEM INTO SINGLE LIST
-;;takes a list of lists and returns the contents of all singletons as a list 
+;; Takes a 2d list of lists and returns the contents of all singletons as a list 
 (define (find-singleton lst)
   (acc-find-singleton lst '()))
 
@@ -16,20 +16,12 @@
    [(= (length (car lst)) 1)  (acc-find-singleton (cdr lst) (append resLst (car lst)  ))]
    [else (acc-find-singleton (cdr lst) resLst)]))
   
-(define (remove-singleton-from-sets matrix)
-  (map (lambda (x)
-         (cond
-           [(= (length x) 1) x]
-           [else (remove* (find-singleton matrix) x)]))
-         matrix))
 
 
 ;;Finds singleton in column
 (define (find-singleton-column item-no matrix)
-  (find-singleton (map (lambda (x) (get-item item-no x)) matrix)))
-
-
-
+  (find-singleton 
+   (map (lambda (x) (get-item item-no x)) matrix)))
 
 
 ;; SETS UP THE SUDOKU  
@@ -42,104 +34,46 @@
             matrix))
 
 ;;SOLVES THE SUDOKU
-(define (solve matrix)
+(define (filter-rows matrix)
   (map (lambda (x) 
-         (remove-singleton-from-sets x)) 
+         (filter-rows-helper x)) 
        matrix))
 
-
-
-;;TEST AREA
-
-
-(define (remove-if-non-singleton lst-to-remove lst)
-  (if (> (length lst) 1) 
-      (remove* lst-to-remove lst)
-      lst)) 
+(define (filter-rows-helper matrix)
+  (map (lambda (x)
+         (cond
+           [(= (length x) 1) x]
+           [else (remove* (find-singleton matrix) x)]))
+         matrix))
 
 
 
-;(define (remove-col-dupe matrix)
- ; (map (lambda (indexed-matrix) 
-  ;       (remove-col-dupe-helper indexed-matrix matrix))
-   ;    (add-column-index matrix)))
 
 
+(define (filter-columns2 indexed-matrix matrix) 
+  (append(map         
+      (lambda (x)    (remove-if-non-singleton (find-singleton-column (cadr x) matrix) (car x)))
+  indexed-matrix)))
 
-;(define (remove-col-dupe-helper indexed-matrix matrix)
- ; (map (lambda (x) 
-  ;      (print (remove-if-non-singleton (find-singleton-column (cadr x) matrix) (car x)))
-   ;      (car x))
-    ;   indexed-matrix))
-  
-(define (remove-col-dupe matrix)
-  (acc-remove-col-dupe matrix (add-column-index matrix)'()))
-
-
-(define (acc-remove-col-dupe matrix indexed-matrix acc)
-  (cond
-    [(empty? indexed-matrix) acc]
-    [(number? (cdr indexed-matrix)) print (cadr (indexed-matrix))]
-    [else (acc-remove-col-dupe matrix (cdr indexed-matrix) acc)])
+(define (filter-columns matrix)
+  (append
+   (map 
+    (lambda (x) (filter-columns2 x matrix))
+    (add-column-index matrix)
+    )
+   )
   )
 
 
-(define (JOOLSTEST2 lst) 
-  (append(map 
-   (lambda (x) (cdr x))
-  lst)))
 
-(define (JOOLSTEST matrix)
-  (append(map 
-                   (lambda (x) (JOOLSTEST2 x)) 
-                       (add-column-index matrix)
-                       )))
-
-
-(JOOLSTEST (solve(transform matrix)))
-
-
-
-
-  
-  ;(remove-col-dupe (solve(transform matrix)))
-  
-  
-
-  
-
-
-;;;;TEST AREA
-
-;;START HERE NEED TO APPLY SOLVE METHODS TO DIFFERENT LIST STRUCTURE
-;; these are the inner element after ---> (add-column-index (transform matrix))
- (define TESTLIST (list (list 1 2 3 4 5 6 7 8 9) 0))
-;; base case is (pair? (cadr TESTLIST)) which is the index
-
-
-(define JOOLS (list(list(list 1 2 3 4 5 6 7 8 9) 0) (list (list 2) 1) (list(list 5) 2) (list( list 1 2 3 4 5 6 7 8 9) 3) (list (list 1 2 3 4 5 6 7 8 9) 4) (list (list 1) 5) (list ( list 1 2 3 4 5 6 7 8 9) 6) (list (list 1 2 3 4 5 6 7 8 9) 7) (list (list 1 2 3 4 5 6 7 8 9) 8)))
-;;;;END OF TEST AREA
-
-
-
-
-;(solve(add-column-index (transform matrix)))
-
-(define TESTY (add-column-index (solve(transform matrix))))
-
-;(transform matrix)
-;(solve (transform matrix))
-;(add-column-index (solve (transform matrix)))
-
-;(remove-col-dupe (solve(transform matrix)))
-
-
+;;EXECUTE HERE
+(filter-columns (filter-rows(transform matrix)))
 
 
 
 ;;;NOTES TO DELETE
-; (member "fall" (list "hop" "skip" "jump"))
+
 ;  once all sets of numbers are reduced to length 1, flatten each row into original format of Matrix
 ;  http://stackoverflow.com/questions/15871042/how-do-i-find-the-index-of-an-element-in-a-list-in-racket
-; (list-index (curry equal? 2) (car (transform matrix)))
+
 ;;END OF NOTES TO DELETE
